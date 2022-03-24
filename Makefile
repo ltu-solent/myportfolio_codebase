78c3871e9b9df143c9526c4a45afbfb90126a695
Mark Sharp <mark.sharp@solent.ac.uk>
2022-03-24 09:05:07 +0000

    Include CSS building in Makefile

--- Makefile
+++ Makefile
@@ -8,4 +8,11 @@ build:
 	@rsync -avz src/mahara-artefact_cpds/. build/.
 	@mkdir -p build/local/lang/en.utf8 && rsync -avz src/mahara-local-lang-en/. build/local/lang/en.utf8/.
 	@mkdir -p build/theme/solent && rsync -avz src/mahara-theme-solent/. build/theme/solent/.
+	@rm build/theme/default/.gitignore
+	@rm build/theme/maroon/.gitignore
+	@rm build/theme/modern/.gitignore
+	@rm build/theme/ocean/.gitignore
+	@rm build/theme/primaryschool/.gitignore
+	@rm build/theme/raw/.gitignore
+	gulp css
 	@rsync -avz src/chrome-php build/lib
---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+// Include gulp
+var gulp = require('gulp');
+
+//Polyfill so we don't need >= node 0.12
+require('es6-promise').polyfill();
+
+// Include Our Plugins
+var sass = require('gulp-sass')(require('node-sass'));
+var path = require('path');
+var cleanCSS = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
+var es = require('event-stream');
+var globule = require('globule');
+var argv = require('yargs').default('production', 'true').argv;
+var gulpif = require('gulp-if');
+
+// Locate all the themes (they're the directories with a "themeconfig.php" in them)
+var themes = globule.find('build/theme/*/themeconfig.php');
+themes = themes.map(function(themepath){
+    themepath = path.join(themepath, '..');
+    return themepath;
+});
+
+// Turn sass into css
+async function css () {
+    var tasks = themes.map(function(themepath){
+
+        console.log("Compiling CSS for " + themepath);
+        return gulp.src('sass/**/*.scss', {cwd: themepath})
+            .pipe(gulpif(argv.production !== 'false', sass().on('error', sass.logError), sass({
+                style: 'expanded',
+                sourceComments: 'normal'
+            }).on('error', sass.logError)))
+            .pipe(autoprefixer({
+              browsers: ['last 4 version'],
+              cascade: false
+            }))
+            .pipe(gulpif(argv.production !== 'false', cleanCSS()))
+            .pipe(gulp.dest('style/', {cwd: themepath}));
+    });
+
+    return es.concat.apply(null, tasks);
+};
+
+// function watch() {
+//     gulp.watch('htdocs/theme/*/sass/**/*.scss', gulp.series('css'));
+// }
+
+gulp.task('css', css);
+// gulp.task('watch', watch);
+
+gulp.task('default', gulp.series(css));
