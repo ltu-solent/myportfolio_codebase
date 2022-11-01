7543af2ffd44f21ae9a73ddb71f6f377d6360abf
Mark Sharp <mark.sharp@solent.ac.uk>
2022-11-01 12:11:24 +0000

    Missing language string

---
+++ build/local/lang/en.utf8/admin.php
@@ -0,0 +1,4 @@
+<?php
+defined('INTERNAL') || die();
+
+$string['newregistrationpolicyinfo'] = '<ul><li>We have updated our registration data policy to require sites to opt out of providing weekly updates of anonymous data to gain better insight into how Mahara is used.</li><li>We have altered what anonymous data we collect.</li></ul>';
